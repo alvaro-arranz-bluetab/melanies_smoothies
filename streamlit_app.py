baa8c9e211e851cef4823ff262c10970d124d41e
alvaro-arranz-bluetab <alvaro.arranz@bluetab.net>
2024-06-12 12:45:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,7 @@ if ingredients_list:
         st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
         
         st.subheader(fruit_chosen + 'Nutrition Information')
+        st.write(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
         fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
